9b4ac6a295c1344e208ef9d4f16a5ebc689822e6
KCN <realkcn@gmail.com>
2015-02-04 11:31:30 +0800

    add config

--- check.py
+++ check.py
@@ -1,24 +1,53 @@
 # -*- coding: gbk -*-
-import sys, time
+import time
+import configparser
+
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
+
 
 __author__ = 'kcn'
 
 driver = webdriver.Firefox()
 
+config = configparser.ConfigParser()
+with open('/etc/kcn.conf', 'r') as cfgfile:
+    config.read_file(cfgfile)
+    username = config.get('moofeel', 'username')
+    password = config.get('moofeel', 'password')
+
+
 def start():
     driver.get("http://www.moofeel.com/forum.php")
     assert "磨坊" in driver.title
 
+
 def login():
+    count = 0
     elem = driver.find_element_by_id("ls_username")
-    elem.send_keys("username")
+    elem.send_keys(username)
     elem = driver.find_element_by_id("ls_password")
-    elem.send_keys("password")
-    time.sleep(5)
-    submitbox = driver.find_element_by_css_selector("#lsform button[type=\"submit\"]")
-    submitbox.click()
+    elem.send_keys(password)
+    # 点击登录，尝试3次
+    while count < 3:
+        count += 1
+        time.sleep(3)
+        try:
+            submitbox = driver.find_element_by_css_selector("#lsform button[type=\"submit\"]")
+        except NoSuchElementException:
+            break
+        submitbox.click()
+        try:
+            nick = driver.find_element_by_css_selector("#um a[title=\"访问我的空间\"]")
+            if (nick != None) and (nick.text == username):
+                break
+        except NoSuchElementException:
+            continue
+    if count >= 3:
+        driver.save_screenshot("/tmp/mofangloginerror.jpg")
+        print("login error")
+        raise Exception
+
 
 def goto_check():
     driver.get("http://www.moofeel.com/forum-96-1.html")
@@ -26,8 +55,9 @@ def goto_check():
     normalthreads = driver.find_elements_by_css_selector("#moderate th.new a")
 
     normalthread = normalthreads[0]
-    print("click %s" % (normalthread.get_attribute("href")))
+    # print("click %s" % (normalthread.get_attribute("href")))
     normalthread.click()
+
 
 def do_reply():
     editbox = driver.find_element_by_id("fastpostmessage")
@@ -36,9 +66,17 @@ def do_reply():
     submitbutton = driver.find_element_by_id("fastpostsubmit")
     submitbutton.submit()
 
+
 def click_get():
+    credit = driver.find_element_by_id("hcredit_3")
+    oldcredit = credit.text
     img = driver.find_element_by_css_selector("img[alt=\"回复帖子签到后，点这领取 3 磨币\"]")
     img.click()
+    time.sleep(10)
+    credit = driver.find_element_by_id("hcredit_3")
+    newcredit = credit.text
+    print("磨币从%s增长到%s" % (oldcredit, newcredit))
+
 
 start()
 login()
