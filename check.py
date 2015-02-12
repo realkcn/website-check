7ccd7cd339d84ea0a811000b26b8e86d725177dd
realkcn <realkcn@gmail.com>
2015-02-12 20:14:42 +0800

    用multiprocessing处理多进程

--- check.py
+++ check.py
@@ -6,30 +6,44 @@ from sites import *
 import subprocess
 import io,time
 from contextlib import redirect_stdout
-
+from multiprocessing import Pool
 
 __author__ = 'kcn'
 
 def checkit(sitename):
-    driver = webdriver.Firefox()
+    with io.StringIO() as buf, redirect_stdout(buf):
+        errorinfo = None
+        try:
+            driver = webdriver.Firefox()
+            eval(sitename + ".checkit(driver)")
+        except Exception as e:
+            errorinfo = e
+        finally:
+            sys.stdout = sys.__stdout__
+            print("%s output:" % sitename)
+            print(buf.getvalue())
+            if errorinfo:
+                print("Exception: %s" % e)
     # driver = None
-    eval(sitename + ".checkit(driver)")
 
-for site in sites.__all__:
-    child = os.fork()
-    if child == 0:
-        with io.StringIO() as buf, redirect_stdout(buf):
-            errorinfo = None
-            try:
-                checkit(site)
-            except Exception as e:
-                errorinfo = e
-            finally:
-                sys.stdout = sys.__stdout__
-                print("%s output:" % site)
-                print(buf.getvalue())
-                if errorinfo:
-                    print("Exception: %s" % e)
-    else:
-        pid, status = os.waitpid(child, 0)
+with Pool(processes=4) as pool:
+    pool.map(checkit, sites.__all__)
+
+# for site in sites.__all__:
+    # child = os.fork()
+    # if child == 0:
+    #     with io.StringIO() as buf, redirect_stdout(buf):
+    #         errorinfo = None
+    #         try:
+    #             checkit(site)
+    #         except Exception as e:
+    #             errorinfo = e
+    #         finally:
+    #             sys.stdout = sys.__stdout__
+    #             print("%s output:" % site)
+    #             print(buf.getvalue())
+    #             if errorinfo:
+    #                 print("Exception: %s" % e)
+    # else:
+    #     pid, status = os.waitpid(child, 0)
         # print("wait returned, pid = %d, status = %d" % (pid, status))
